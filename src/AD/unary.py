4a09b48847c7e8be24e99195d6c25fb105da110b
Katie Grosch <katie.grosch@Katies-MacBook.local>
2019-11-05 19:07:36 -0500

    fetched remote unary.py

--- src/AD/unary.py
+++ src/AD/unary.py
@@ -28,42 +28,42 @@ def cos(x):
 
 def tan(x):
 	try:
-		return ad.AD(np.tan(x.val), 1 / (cos(x.val) * cos(x.val)))
+		return ad.AD(np.tan(x.val), x.der*(np.cos(x.val)**2+np.sin(x.val)**2)/(np.cos(x.val)**2))
 	except AttributeError:
 		return np.tan(x)
 
 def arcsin(x):
 	try:
-		return ad.AD(np.arcsin(x.val), 1 / sqrt(1 - x.val**2))
+		return ad.AD(np.arcsin(x.val), x.der/np.sqrt(1-x.val**2))
 	except AttributeError:
 		return np.arcsin(x)
 
 def arccos(x):
 	try:
-		return ad.AD(np.arccos(x.val), -1 / sqrt(1 - x.val**2))
+		return ad.AD(np.arccos(x.val), -1*x.der/np.sqrt(1-x.val**2))
 	except AttributeError:
 		return np.arccos(x)
 
 def arctan(x):
 	try:
-		return ad.AD(np.arctan(x.val), 1 / (1 + x.val**2))
+		return ad.AD(np.arctan(x.val), x.der/(1+x.val**2))
 	except AttributeError:
 		return np.arctan(x)
 
 def sinh(x):
 	try:
-		return ad.AD(np.sinh(x.val), np.cosh(x.val))
+		return ad.AD(np.sinh(x.val), x.der*np.cosh(x.val))
 	except AttributeError:
 		return np.sinh(x)
 
 def cosh(x):
 	try:
-		return ad.AD(np.cosh(x.val), np.sinh(x.val))
+		return ad.AD(np.cosh(x.val), x.der*np.sinh(x.val))
 	except AttributeError:
 		return np.cosh(x)
 
 def tanh(x):
 	try:
-		return ad.AD(np.tanh(x.val), 1 / (np.cosh(x.val))**2)
+		return ad.AD(np.tanh(x.val), x.der*(1-np.tanh(x.val)**2))
 	except AttributeError:
 		return np.tanh(x)
