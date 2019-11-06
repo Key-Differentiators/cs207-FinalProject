d56e20cf29855e915923ddf7c1ddec217ff2c860
Katie Grosch <katie.grosch@Katies-MacBook.local>
2019-11-06 14:27:16 -0500

    changed re import to regex

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 # setup.py
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
-    name='KeyDifferentiator',
+    name='keydifferentiator',
     url='https://github.com/Key-Differentiators/cs207-FinalProject',
     author='Kate Grosch, Spencer Penn, Mingyue Wei',
-    packages=find_packages(exclude=("tests",)),
-    install_requires=['numpy', 're'],
+    packages=find_packages(exclude=("tests")),
+    install_requires=['numpy', 'regex'],
     version='0.1',
     license='MIT',
     description='Automatic differentiation tool',
