535196fe35f2b0da6225b47a2f9171a07f303662
Katie Grosch <katie.grosch@Katies-MacBook.local>
2019-11-11 15:48:44 -0500

    fixed floating pt issue

--- keydifferentiator/__main__.py
+++ keydifferentiator/__main__.py
@@ -8,11 +8,17 @@ def main():
 	while True:
 		f = input("Input your function:   f(x) = ")
 		x = input("What value of x would you like to evaluate at? Enter a number:   ")
-		print("Evaluating f(x) = %s at x = %d..." % (f, int(x)))
-		f = re.sub('x', 'ad.AD(x)', f)
-		f = re.sub('x', x, f)
-		x = eval(f)
-		print(x)
+		print("Evaluating f(x) = %s at x = %f..." % (f, float(x)))
+		eq = re.sub('x', 'ad.AD(x)', f)
+		eq = re.sub('x', x, eq)
+		try: 
+			x = eval(eq)
+			print(x)
+		except:
+			print("Cannot evaluate f(x) = %s at x = %d." % (f, float(x)))
+			print("Hints: ")
+			print("    - Use * for multiplication (e.g. 3*x, not 3x)")
+			print("    - Use ** for exponentiation (e.g. 3**x, not 3^x)")
 
 if __name__ == "__main__":
 	main()
